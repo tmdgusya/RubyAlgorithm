2a09c1d0da9ecfecc20b477ee96564a9821f8a3d
roach <tmdgusya@naver.com>
2021-06-08 11:33:43 +0900

    leetcode: RemoveElement

--- leetcode/test_remove_element.rb
+++ leetcode/test_remove_element.rb
@@ -4,11 +4,16 @@ class TestRemoveElement < Test::Unit::TestCase
 
     def test_remove_element
         assert_equal(self.remove_element([3,2,2,3], 3), 2)
+        assert_equal(self.reject_elemente([3,2,2,3], 3), 2)
     end
 
     def remove_element(nums, val)
         nums.delete(val)
         nums.length
+    end
+    
+    def reject_elemente(nums, val)
+        nums.reject{|v| v == val}.length
     end
 
 end
